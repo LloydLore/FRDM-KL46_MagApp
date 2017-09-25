b269183505814eeb9305a38ad2f7c81caf749e04
LloydLore <15190010740@163.com>
2017-09-25 10:48:10 +0800

    1st commit. 9/25/2017. contains: basic UART to PC communicatoin.

---
+++ FLASH/Generated_Code/AS1.d
@@ -0,0 +1,119 @@
+Generated_Code/AS1.o: ../Generated_Code/AS1.c \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Generated_Code/AS1.h ../Generated_Code/Cpu.h \
+ ../Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/IO_Map.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/IO_Map/MKL25Z4.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Sources/Events.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1config.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBufconfig.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1config.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/UART0_PDD.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/PDD_Types.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/SIM_PDD.h
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Generated_Code/AS1.h:
+
+../Generated_Code/Cpu.h:
+
+../Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/IO_Map/MKL25Z4.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Sources/Events.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1config.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBufconfig.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1config.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/UART0_PDD.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/PDD_Types.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/PDD/SIM_PDD.h:
---
+++ FLASH/Sources/main.d
@@ -0,0 +1,109 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/IO_Map/MKL25Z4.h \
+ ../Sources/Events.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1config.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBufconfig.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1config.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h \
+ ../Sources/Application.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_LDD.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h \
+ C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.7/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Static_Code/IO_Map/MKL25Z4.h:
+
+../Sources/Events.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Types.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Error.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_Const.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/IO_Map.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/Cpu.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1config.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBufconfig.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1config.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/MCUC1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/CS1.h:
+
+../Sources/Application.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/PE_LDD.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/AS1.h:
+
+C:/Users/michael.luo/workspace/FRDM-KL25Z\ UART/Generated_Code/RxBuf.h:
